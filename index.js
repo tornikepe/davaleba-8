19e63ce34c65ac7acfab2848846f23c2a44d38a5
tornikepe <peitrishvilitornike@gmail.com>
2024-03-27 13:28:07 +0400

    davaleba-8

---
+++ index.js
@@ -0,0 +1,40 @@
+var x = 87;
+var y = 65;
+console.log(x + y);
+
+var student = {
+  firstName: "Tornike",
+  lastName: "Peitrishvili",
+  age: 21,
+  email: "peitrishvilitornike@gmail.com",
+  marks: "test",
+};
+console.log(student.firstName);
+
+var m = 45;
+var n = 23;
+if (m > n) {
+  console.log(n + m);
+} else {
+  console.log(n);
+}
+
+var studentName = "Tornike";
+console.log("Rise and Shine,", studentName);
+
+var bonus = 2500;
+var salary = 4500;
+if (salary >= 2000) {
+  bonus = 0;
+} else {
+  bonus = salary * 0.1;
+}
+console.log(bonus);
+
+var a = 23;
+var b = a / 2;
+var c = a + b;
+console.log(c);
+
+var phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+console.log(phones);
